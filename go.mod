cd4186bebff4925e10dccdd0a14515adc8dfbba2
kr19al0711 <kripalparsekar@gmail.com>
2026-01-27 11:41:00 +0000

    added form input validation and returned results.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anxious-aurelius/unit-converter
+
+go 1.25.6
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type formData struct {
@@ -11,7 +14,6 @@ type formData struct {
 }
 
 func homeHandler(rw http.ResponseWriter, _ *http.Request) {
-
 	lengthData := formData{unitToString()}
 	rw.Header().Set("Content-Type", "text/html; charset=utf-8")
 	files, err := template.ParseFiles("./templates/home.html")
@@ -22,9 +24,35 @@ func homeHandler(rw http.ResponseWriter, _ *http.Request) {
 	files.Execute(rw, lengthData)
 }
 
+func convertHandler(rw http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Fetch the form values from the name field in html tag and validate them before passing  them to the function
+	value, err := strconv.ParseFloat(strings.TrimSpace(r.FormValue("value")), 64)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+	}
+	toUnit, err := stringToUnit(strings.TrimSpace(r.FormValue("to")))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+	}
+	fromUnit, err := stringToUnit(strings.TrimSpace(r.FormValue("from")))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+	}
+	convertedValue, err := convertLengthUnit(value, fromUnit, toUnit)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+	fmt.Fprintf(rw, "Result: %f", convertedValue)
+}
+
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("GET /", homeHandler)
+	mux.HandleFunc("POST /convert", convertHandler)
 	log.Fatal(http.ListenAndServe("localhost:8080", mux))
 }
